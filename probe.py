213a1744f9d2078ce6a9e2345867417b4e279c5e
gvozdovskiyAlexey <test.akk.android@gmail.com>
2023-03-10 15:11:41 +0800

    Add and complete homework task_1.1

---
+++ probe.py
@@ -0,0 +1,22 @@
+letter = str
+word = input("введите текст: ")
+
+while letter != "стоп":
+  letter = input("введите искомый символ (или стоп для выхода): ")
+  count = 0
+  nums = []
+  pos = 0
+
+  for L in word:
+    pos += 1
+    if L == letter:
+      count = word.count(L)
+      nums.append(pos)
+
+  if letter == "стоп":
+    print("поиск остановлен!")
+    break
+  elif count == 0:
+    print("символа", letter, "нет в тексте")
+  elif count != 0:
+    print("символ", letter, "встречается", count, "раз(а) на", nums, "позиции")
---
+++ task_1.1.py
@@ -0,0 +1,29 @@
+# Задача 1.1.
+
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+spltd_sngs = my_favorite_songs.split(', ')
+print(spltd_sngs[0], spltd_sngs[-1], spltd_sngs[1],spltd_sngs[-2])
+
+
+
+print('''
+
+
+#а это был первый вариант решения''')
+nums = []
+pos = 0
+
+for smbl in my_favorite_songs:
+  pos += 1
+  if smbl == ',':
+    nums.append(pos)
+
+print(my_favorite_songs[:nums[0] - 1])
+print(my_favorite_songs[nums[-1] + 1:-1])
+print(my_favorite_songs[nums[0] + 1:nums[1] - 1])
+print(my_favorite_songs[nums[1] + 1:nums[2] - 1])
