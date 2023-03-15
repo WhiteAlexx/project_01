403fcd992dfc93392383aeaba3ba5db08b5631af
gvozdovskiyAlexey <test.akk.android@gmail.com>
2023-03-15 15:28:13 +0800

    update file

--- task_1.3.py
+++ task_1.3.py
@@ -35,3 +35,44 @@ elif num_mnth <= 7 and num_mnth % 2 == 1 or num_mnth >= 8 and num_mnth % 2 == 0:
 
 else:
     print('В этом месяце 30 дней')
+
+
+print('''
+    
+    Вариант с названием месяца
+    ''')
+
+def select_month(number):
+    months = {
+        1: "Январь",
+        2: "Февраль",
+        3: "Март",
+        4: "Апрель",
+        5: "Май",
+        6: "Июнь",
+        7: "Июль",
+        8: "Август",
+        9: "Сентябрь",
+        10: "Октябрь",
+        11: "Ноябрь",
+        12: "Декабрь"
+    }
+    name_mnth = months[number]
+    return name_mnth
+
+num_mnth = int(input('Введите номер месяца: '))
+
+if num_mnth < 1 or num_mnth > 12:
+    print('Такого месяца нет!')
+
+elif num_mnth == 2:
+    name_mnth = select_month(num_mnth)
+    print('Выбран ', name_mnth, '. В этом месяце 28 дней', sep="")
+
+elif num_mnth <= 7 and num_mnth % 2 == 1 or num_mnth >= 8 and num_mnth % 2 == 0:
+    name_mnth = select_month(num_mnth)
+    print('Выбран ', name_mnth, '. В этом месяце 31 день', sep="")
+
+else:
+    name_mnth = select_month(num_mnth)
+    print('Выбран ', name_mnth, '. В этом месяце 30 дней', sep="")
